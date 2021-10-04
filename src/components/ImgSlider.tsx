3fdecfb50dec8fb3224d2d8f4ecc09b5249a36d3
Yang Jae <remd@naver.com>
2021-10-04 15:16:34 +0900

    2021-10-04 디즈니 뷰어

--- src/components/ImgSlider.tsx
+++ src/components/ImgSlider.tsx
@@ -56,7 +56,7 @@ const Wrap = styled.div`
     img {
         border: 4px solid transparent;
         border-radius: 4px;
-        width: 100%;
+        width: 99%;
         height: 100%;
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
